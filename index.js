4aa1e8f659cebf06b1e3ecc120c5e777e268a931
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-11-23 05:21:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ let element = document.createElement("div");
 let name = prompt("ur name?");
 element.innerHTML = "<h1> Hello, " + name + "</h1> <p> i may order a coffee today with cookies beside ..</p> <h3> thank you ! </h3>";
 document.body.appendChild(element);
+if(name === "hiba"){
+  alert("i knew earlier that your fav color is purple huh?");
+  document.body.style.backgroundColor = "purple";
+  document.body.style.color = "white";  
+}
