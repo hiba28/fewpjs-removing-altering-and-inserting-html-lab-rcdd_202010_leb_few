052cc2486366f48201f4678e25b7fb23da3cd107
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-11-23 05:19:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code here!
 let element = document.createElement("div");
 let name = prompt("ur name?");
-element.innerHTML = "<h1> Hello, ${name}</h1> <p> i may order a coffee today with cookies beside ..</p> <h3> thank you ! </h3>";
+element.innerHTML = "<h1> Hello, " + name + "</h1> <p> i may order a coffee today with cookies beside ..</p> <h3> thank you ! </h3>";
 document.body.appendChild(element);
