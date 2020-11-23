da05f7263aec70337e31b699396c803eaf5642a0
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-11-23 05:21:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ element.innerHTML = "<h1> Hello, " + name + "</h1> <p> i may order a coffee toda
 document.body.appendChild(element);
 if(name === "hiba"){
   alert("i knew earlier that your fav color is purple huh?");
-  document.body.style.backgroundColor = "purple";
-  document.body.style.color = "white";
+  if(reply === "yes"){
+    document.body.style.backgroundColor = "purple";
+    document.body.style.color = "white";
+  }
 }
